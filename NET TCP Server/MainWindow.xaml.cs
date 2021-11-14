b1416f789f458e3cdf40d9fe8ef23452d6ec5ea7
Ko1ors <kolorsnik181200@gmail.com>
2021-11-14 21:26:38 +0200

    Add project files.

---
+++ NET TCP Server/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace NET_TCP_Server
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var homePage = Properties.Resources.home;
+
+            var server = new TcpServer(IPAddress.Parse("127.0.0.1"), 4444, homePage);
+            Task.Run(() => server.Start());
+        }
+    }
+}
---
+++ NET TCP Server/TcpServer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_TCP_Server
+{
+    public class TcpServer
+    {
+        private TcpListener Listener { get; set; }
+
+        private string Page { get; set; }
+
+        public TcpServer(IPAddress address, int port, string page)
+        {
+            Page = page;
+            Listener = new TcpListener(address, port);
+        }
+
+        public void Start()
+        {
+            try
+            {
+                Listener?.Start();
+                while (true)
+                {
+                    TcpClient client = Listener.AcceptTcpClient();
+                    StreamWriter writer = new System.IO.StreamWriter(client.GetStream());
+                    writer.Write("HTTP/1.0 200 OK");
+                    writer.Write(Environment.NewLine);
+                    writer.Write("Content-Length: " + Page.Length);
+                    writer.Write(Environment.NewLine);
+                    writer.Write(Environment.NewLine);
+                    writer.Write(Page);
+
+                    writer.Flush();
+                    client.Close();
+                }
+            }
+            catch
+            {
+                Stop();
+            }
+        }
+
+        public void Stop()
+        {
+            Listener?.Stop();
+        }
+    }
+}
